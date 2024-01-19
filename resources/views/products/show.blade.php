801eb63ad942d86536525b37a900724ebaf0c6f9
MostafaGhidanM <mostafaghidannti26nov@gmail.com>
2024-01-19 21:39:52 +0200

    show product blade and controller

---
+++ resources/views/products/show.blade.php
@@ -0,0 +1,41 @@
+@extends('products.productLayout')
+@section('content')
+@section("content")
+<div class="row">
+    <div class="col-lg-12 margin-tb">
+        <div class="pull-left">
+            <h2>Show Product</h2>
+        </div>
+        <div class="pull-right">
+            <a class="btn btn-primary" href="{{ route('products.index') }}">Home Page</a>
+        </div>
+    </div>
+</div>
+
+<div class="row">
+    <div class="col-xs-12 col-sm-12 col-md-12">
+        <div class="form-group">
+            <strong>Product Name:</strong>
+            {{ $product->ProductName }}
+        </div>
+    </div>
+    <div class="col-xs-12 col-sm-12 col-md-12">
+        <div class="form-group">
+            <strong>Price:</strong>
+            {{ $product->productPrice }}
+        </div>
+    </div>
+    <div class="col-xs-12 col-sm-12 col-md-12">
+        <div class="form-group">
+            <strong>Description:</strong>
+            {{ $product->productDescription }}
+        </div>
+    </div>
+    <div class="col-xs-12 col-sm-12 col-md-12">
+        <div class="form-group">
+            <strong>Producer:</strong>
+            {{ $product->productProducer }}
+        </div>
+    </div>
+</div>
+@endsection
